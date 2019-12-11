b5c5bed0f8b9a72fbe75245be4f3d4111b009664
NeuroticOutsider <klimberg-nikita@yandex.ru>
2019-12-11 16:11:19 +0300

    About section done + adaptive

--- js/common.js
+++ js/common.js
@@ -51,4 +51,23 @@ $(function() {
 		};
 	});
 
+  // Accordion (section about)
+
+  if ($('.accordion__btn').hasClass('accordion__btn--active')) {
+    $('.accordion__btn--active i').removeClass('fa-plus').addClass('fa-minus');
+  } else {
+    $('.accordion__btn--active i').removeClass('fa-minus').addClass('fa-plus');
+  }
+
+  $('.accordion__btn').click(function() {
+    $('.accordion__btn').removeClass('accordion__btn--active');
+    $(this).addClass('accordion__btn--active');
+
+    $('.accordion__btn i').removeClass('fa-minus').addClass('fa-plus');
+    $('.accordion__btn--active i').addClass('fa-minus').removeClass('fa-plus');
+
+    $('.accordion__text').not($(this).next()).slideUp();
+    $(this).next().slideToggle();
+  });
+
 });
