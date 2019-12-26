e3743967a89d219ec4c7f1b2e12581fcf1860208
NeuroticOutsider <klimberg-nikita@yandex.ru>
2019-12-26 14:27:02 +0300

    Options section done + adaptive

--- js/common.js
+++ js/common.js
@@ -2,40 +2,12 @@ $(function() {
 
   // Header slider
 
-  $('#header-slider').slick({
-    // slidesToScroll: 4,
-    // responsive: [
-    //   {
-    //     breakpoint: 1024,
-    //     settings: {
-    //       slidesToShow: 3,
-    //       slidesToScroll: 3,
-    //       infinite: true,
-    //       dots: true
-    //     }
-    //   },
-    //   {
-    //     breakpoint: 600,
-    //     settings: {
-    //       slidesToShow: 2,
-    //       slidesToScroll: 2
-    //     }
-    //   },
-    //   {
-    //     breakpoint: 480,
-    //     settings: {
-    //       slidesToShow: 1,
-    //       slidesToScroll: 1
-    //     }
-    //   }
-    // ]
-  });
+  $('#header-slider').slick();
 
   // first word title add in span
 
   $('.section__title').each(function() {
-		var ths = $(this);
-		ths.html(ths.html().replace(/^(\S+)/, '<span>$1</span>'));
+		$(this).html($(this).html().replace(/^(\S+)/, '<span>$1</span>'));
 	});
 
   // Hamburger-menu
